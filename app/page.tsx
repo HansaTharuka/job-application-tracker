1fbe63353bdc500e556df42dbd483dc32b09ec92
Prodigi Interactive Developer <prodigi.dev@gmail.com>
2026-03-16 23:49:21 +0530

    fixed keyword

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import {ArrowRight} from "lucide-react"
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("organized"); //organized, hired, boards
   return (
     <div className="flex min-h-screen flex-col bg-white">
       <main className="flex-1">
@@ -15,12 +20,53 @@ export default function Home() {
               Capture, Organize and Manage your job applications in one place.
             </p>
             <div className="flex flex-col items-center gap-4">
-              <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                Start for free
-              </Button>
+              <Link href="/sign-up">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                  Start for free <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p className="text-sm text-muted-forground">
                 Free forever. No credit card required.
               </p>
+            </div>
+          </div>
+        </section>
+        {/* Hero Images Section */}
+        <section className="border-t bg-white py-16"> 
+          <div className="container mx-auto px-4">
+            <div className="mx-auto mx-w-6xl">
+              {/* Tabs Image */}
+              <div className="flex gap-2 justify-center b-8">
+                <Button onClick={() => setActiveTab("organized")}>Organize Applications</Button>
+                <Button onClick={() => setActiveTab("hired")}>Get Hired</Button>
+                <Button onClick={() => setActiveTab("boards")}>Manage boards</Button>
+              </div>
+              <div className="relative mx-auto max-w-5xl overflow-hiden rounded-lg border-gray-200 shadow-xl">
+                {activeTab === "organized" && (
+                  <Image
+                    src="/hero-images/hero1.png"
+                    alt="Organize Application"
+                    width={1200}
+                    height={800}
+                  />
+                )}
+                 {activeTab === "hired" && (
+                <Image
+                  src="/hero-images/hero2.png"
+                  alt="Organize Application"
+                  width={1200}
+                  height={800}
+                />
+                 )}
+                 {activeTab === "boards" && (
+                <Image
+                  src="/hero-images/hero3.png"
+                  alt="Organize Application"
+                  width={1200}
+                  height={800}
+                />
+                 )}
+              </div>
             </div>
           </div>
         </section>
